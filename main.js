3d5313d3a2d351e02ede74fc4a3b6426d2e3139a
Daniel Benýšek <daniel@benysek.cz>
2023-11-17 11:34:53 +0100

    Added new function!

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let qrImg = document.getElementById('qrImg');
-  let download = document.getElementById('download');
+let qrImg = document.getElementById('qrImg');
+let download = document.getElementById('download');
+let input = document.getElementById('input');
+
+document.addEventListener('DOMContentLoaded', function() {    
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {    
+        pageUrl = tabs[0].url;
+    
+        input.placeholder = pageUrl;
   
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {    
-  url = tabs[0].url;
-  const pageTitle = tabs[0].title;
-  const parts = url.split(/[@&#]/);
-  const baseUrl = parts[0];
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            imageDark(pageUrl);
+        } else {
+            imageLight(pageUrl);
+        };
+        download.addEventListener('click', downloadFile(pageUrl));
 
-  download.addEventListener('click', function() {
-    download.download = `${pageTitle}-qr_code.png`;
-    downloadFile(`https://api.qrserver.com/v1/create-qr-code/?data=${baseUrl}&format=png&size=512x512`, `${pageTitle}-qr_code.png`);
-  });
-
-  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?data=${baseUrl}&bgcolor=1f1f1f&color=d1d1d1&format=png&size=200x200`;
-  } else {
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?data=${baseUrl}&bgcolor=fff&color=1f1f1f&format=png&size=200x200`;
-  }
-
-  });
+        document.addEventListener("focusout", function() {
+            if (input.value == '') {
+                if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {imageDark(pageUrl)} else {imageLight(pageUrl)};
+                download.addEventListener('click', downloadFile(pageUrl));
+            } else {
+                if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {imageDark(input.value)} else {imageLight(input.value)};
+                download.addEventListener('click', downloadFile(input.value));
+            };
+        });
+    });
 });
 
-function downloadFile(url, fileName) {
-  const link = document.createElement('a');
-  link.href = url;
-  link.download = fileName;
-  link.click();
-}
+function downloadFile(url) {
+    download.href= `https://api.qrserver.com/v1/create-qr-code/?format=png&download=1&size=512x512&data=${url}`;
+};
+function imageDark(url){
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?bgcolor=1f1f1f&color=d1d1d1&format=png&size=256x256&data=${url}`;
+};
+function imageLight(url){
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?bgcolor=fff&color=1f1f1f&format=png&size=256x256&data=${url}`;
+};
