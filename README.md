848e2cf6120e7a5e06190df71dd701b4c8741471
Daniel Benýšek <daniel@benysek.cz>
2023-11-19 20:28:28 +0100

    Small changes

--- README.md
+++ README.md
@@ -1,17 +1,32 @@
+<div align="center">
+
 # Simple QR Generator
-### Generates a QR code based off on what website are you or the text you input
+### Generates a QR code based off on what website are you or the text you input.
 
-![GitHub Repo stars](https://img.shields.io/github/stars/danek309044/simple-qr-generator?style=for-the-badge) ![GitHub all releases](https://img.shields.io/github/downloads/danek309044/simple-qr-generator/total?style=for-the-badge&color=%2314ba14) ![GitHub](https://img.shields.io/github/license/danek309044/simple-qr-generator?style=for-the-badge)
+![GitHub Repo stars](https://img.shields.io/github/stars/danek309044/simple-qr-generator) ![GitHub](https://img.shields.io/github/license/danek309044/simple-qr-generator) ![GitHub manifest version (path)](https://img.shields.io/github/manifest-json/v/danek309044/simple-qr-generator)
 
-## How to install it:
-1. Install the latest relese from [here](https://github.com/danek309044/simple-qr-generator/releases).
-2. Unzip it and place it into a safe folder (in the documents foleder for example)
-3. Go to Chrome or any Chromium based browser and navigate to Extensions or directly go to `chrome://extensions/`.
-4. Enable "Developer Options" in the right top corner.
-5. Click on "Load unpacked extension" and browse to the directory where you extracted the extension.
-6. Now just pin it and use it!
 
-## Screenshots:
-Dark mode | Light mode
-|-------------|-----------|
-| ![dark](https://files.d309044.eu/Yimi1/ZoqurORI65.png/raw) |![light](https://files.d309044.eu/Yimi1/xataTiRe81.png/raw) |
+***
+
+<a href="https://chromewebstore.google.com/detail/simple-gr-generator/himhgbnhhpjahbobhnigcgmipneoihdl"><img src="https://storage.googleapis.com/web-dev-uploads/image/WlD8wC6g8khYWPJUsQceQkhXSlv1/UV4C4ybeBTsZt43U4xis.png"></a>
+
+***
+
+### Screenshots:
+<table>
+    <thead>
+        <tr>
+            <th align="center">Dark mode</th>
+            <th align="center">Light mode</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td align="center" valign="top"><img src="https://files.d309044.eu/Yimi1/DOLUxAZu60.png/raw"></td>
+            <td align="center"><img src="https://files.d309044.eu/Yimi1/VOLiNOZO06.png/raw"></td>
+        </tr>
+    </tbody>
+</table>
+
+
+***
--- main.js
+++ main.js
@@ -1,38 +1,42 @@
+// Get elements by ID
 let qrImg = document.getElementById('qrImg');
 let download = document.getElementById('download');
 let input = document.getElementById('input');
 
-document.addEventListener('DOMContentLoaded', function() {    
+// After Body loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // Get current tab
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {    
         pageUrl = tabs[0].url;
-    
+        
+        // Current url
         input.placeholder = pageUrl;
-  
-        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            imageDark(pageUrl);
-        } else {
-            imageLight(pageUrl);
-        };
+        qrcode(pageUrl)
         download.addEventListener('click', downloadFile(pageUrl));
 
+        // Custom input
         document.addEventListener("focusout", function() {
             if (input.value == '') {
-                if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {imageDark(pageUrl)} else {imageLight(pageUrl)};
+                qrcode(pageUrl)
                 download.addEventListener('click', downloadFile(pageUrl));
             } else {
-                if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {imageDark(input.value)} else {imageLight(input.value)};
+                qrcode(input.value)
                 download.addEventListener('click', downloadFile(input.value));
             };
         });
     });
 });
 
+// Download function
 function downloadFile(url) {
     download.href= `https://api.qrserver.com/v1/create-qr-code/?format=png&download=1&size=512x512&data=${url}`;
 };
-function imageDark(url){
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?bgcolor=1f1f1f&color=d1d1d1&format=png&size=256x256&data=${url}`;
-};
-function imageLight(url){
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?bgcolor=fff&color=1f1f1f&format=png&size=256x256&data=${url}`;
-};
+
+// QR generating function
+function qrcode(url){
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?bgcolor=1f1f1f&color=d1d1d1&format=png&size=200x200&data=${url}`;
+    } else {
+        qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?bgcolor=fff&color=1f1f1f&format=png&size=200x200&data=${url}`;
+    };
+}
